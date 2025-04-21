9be56b82f46128c5107d34c12c3bf3c77ed28b29
Sphere <spherein@hotmail.com>
2025-04-21 16:18:30 +0530

    Cors Fix @1

--- src/app.ts
+++ src/app.ts
@@ -31,7 +31,7 @@ app.use(morgan("dev"));
 app.use(express.json()); 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(cors({ origin: process.env.FRONTEND_URL, credentials: true }));
+app.use(cors({ origin: "*", credentials: true }));
 app.use(passport.initialize());
 app.use(limiter);
 
